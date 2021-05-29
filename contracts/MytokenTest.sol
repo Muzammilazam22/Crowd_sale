9586c6efa010f5befa2cf6f09b1ff4f7378dd818
muzammil <muzammil.azam88@gmail.com>
2021-05-29 13:26:03 +0500

    compiled successfully

--- contracts/MytokenTest.sol
+++ contracts/MytokenTest.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol"
-contract MyTokenTest is ERC20("Rupees","RS") {
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-  _mint(0x4C29Fc8B101f709920b4Fa530c0579e512400E7E,1000*1e18);
+contract MyTokenTest is ERC20("Rupees", "RS") {
+    constructor() public {
+        _mint(0x4C29Fc8B101f709920b4Fa530c0579e512400E7E, 1000 * 1e18);
+    }
 }
